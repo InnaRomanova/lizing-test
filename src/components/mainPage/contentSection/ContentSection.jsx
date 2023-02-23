f5e42aee6512e63f50bc65e157a53ba5d17344e2
unknown <romanovainnap@yandex.ru>
2023-02-23 16:51:35 +0300

    adaptive 320

--- src/components/mainPage/contentSection/ContentSection.jsx
+++ src/components/mainPage/contentSection/ContentSection.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function ContentSection() {
     return (
             <section className="content">
-                <h1 className="content__title">Рассчитайте стоимость автомобиля в лизинг</h1>              
+                <h1 className="content__title">Рассчитайте стоимость автомобиля в&nbsp;лизинг</h1>              
             </section>      
     );
 }
